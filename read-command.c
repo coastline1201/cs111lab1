9dc23753d7163e113e4151c6152ac716b3cf2072
youaregreathk <youaregreathk@gmail.com>
2015-06-28 18:29:57 -0700

    Add stack

--- read-command.c
+++ read-command.c
@@ -2,8 +2,59 @@
 
 #include "command.h"
 #include "command-internals.h"
+#include "alloc.h"
 
+#include <ctype.h>  //For isalpha() and isdigit() functions
+#include <stdlib.h>
+#include <stdio.h>  //For EOF
 #include <error.h>
+#include <string.h>
+#define STACK_MAX 100
+
+
+struct Stack
+{
+  command_t data[STACK_MAX];
+  int size;
+};
+
+
+void Stack_Init(struct Stack *S)
+{
+  S->size = 0;
+}
+
+
+command_t Stack_Top(struct Stack *S)
+{
+  if (S->size == 0) {
+    fprintf(stderr, "Error: stack empty\n");
+    exit(0);
+  } 
+
+  return S->data[S->size-1];
+}
+
+
+void Stack_Push(struct Stack *S, command_t c)
+{
+  if (S->size < STACK_MAX){
+    S->data[S->size] = c;
+    S->size++;
+  }
+        
+  else
+    fprintf(stderr, "Error: stack full\n");
+}
+
+
+void Stack_Pop(struct Stack *S)
+{
+  if (S->size == 0)
+    fprintf(stderr, "Error: stack empty\n");
+  else
+    S->size--;
+}
 
 /* FIXME: You may need to add #include directives, macro definitions,
    static function definitions, etc.  */
@@ -22,10 +73,22 @@ make_command_stream (int (*get_next_byte) (void *),
   return 0;
 }
 
+
 command_t
 read_command_stream (command_stream_t s)
 {
-  /* FIXME: Replace this with your implementation too.  */
-  error (1, 0, "command reading not yet implemented");
-  return 0;
+  if(s != NULL && s->read == 0){
+    command_t tree;
+    tree = s->root;
+    s->read++;
+    return tree;
+  }
+
+  else if(s != NULL && s->next != NULL){
+    return read_command_stream(s->next);
+  }
+
+  else{
+    return NULL;
+  }
 }
